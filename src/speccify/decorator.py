8f1733192d6313826b1c0f9ca41c994ffef1d744
David Szotten <davidszotten@gmail.com>
2021-06-23 20:48:17 +0000

    implementation with drf-dataclasses lib
    we need a custom `build_dataclass_field` so that child-classes are given
    unique names

--- src/speccify/decorator.py
+++ src/speccify/decorator.py
@@ -1,25 +1,43 @@
 import functools
-from dataclasses import asdict, fields
+from dataclasses import asdict
 
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import serializers, status
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework_dataclasses.serializers import DataclassSerializer
+
+
+class CustomDataclassSerializer(DataclassSerializer):
+    def build_dataclass_field(self, field_name: str, type_info):
+        """
+        Create fields for dataclasses.
+        """
+
+        field_class = type(
+            f"DCS__{type_info.base_type.__name__}Serializer", (DataclassSerializer,), {}
+        )
+        field_kwargs = {"dataclass": type_info.base_type, "many": type_info.is_many}
+        return field_class, field_kwargs
 
 
 def foo_api(*, data_class, methods, responses, permissions):
-    field_map = {str: serializers.CharField}
-    serializer_fields = {
-        field.name: field_map[field.type]() for field in fields(data_class)
-    }
-    serializer_cls = type("foo", (serializers.Serializer,), serializer_fields)
+    class RequestDataMeta:
+        dataclass = data_class
+
+    serializer_name = f"{data_class.__name__}Serializer"
+    serializer_cls = type(
+        serializer_name, (CustomDataclassSerializer,), {"Meta": RequestDataMeta}
+    )
 
     response_cls = responses.get(status.HTTP_200_OK)
-    response_serializer_fields = {
-        field.name: field_map[field.type]() for field in fields(response_cls)
-    }
+
+    class ResponseMeta:
+        dataclass = response_cls
+
+    serializer_name = f"{response_cls.__name__}Serializer"
     response_serializer_cls = type(
-        "foo", (serializers.Serializer,), response_serializer_fields
+        "foo", (CustomDataclassSerializer,), {"Meta": ResponseMeta}
     )
 
     def decorator_wrapper(view_func):
@@ -36,14 +54,14 @@ def foo_api(*, data_class, methods, responses, permissions):
             serializer = serializer_cls(data=data)
             serializer.is_valid(raise_exception=True)
 
-            data_instance = data_class(**serializer.validated_data)
+            data_instance = serializer.validated_data
 
             response_data = view_func(request, data_instance)
 
             response_serializer = response_serializer_cls(data=asdict(response_data))
             response_serializer.is_valid(raise_exception=True)
 
-            return Response(status=200, data=response_serializer.validated_data)
+            return Response(status=200, data=asdict(response_serializer.validated_data))
 
         return wrapper
 
--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,5 +1,10 @@
+import json
+import types
 from dataclasses import dataclass
 
+from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import status
 from rest_framework.request import Request
 
@@ -30,3 +35,55 @@ def test_basic(rf):
     request = rf.get("/?name=value")
     response = view(request)
     assert response.data == {"length": "5"}
+
+
+def test_schema(rf):
+    @dataclass
+    class Child1:
+        pass
+
+    @dataclass
+    class Child2:
+        pass
+
+    @dataclass
+    class Parent:
+        child1: Child1
+        child2: Child2
+
+    @foo_api(
+        data_class=Person,
+        methods=["GET"],
+        responses={status.HTTP_200_OK: Parent},
+        permissions=[],
+    )
+    def view(request: Request, person: Person) -> Parent:
+        pass
+
+    urlpatterns = [
+        path("view", view),
+    ]
+    urlconf = types.ModuleType("urlconf")
+    urlconf.urlpatterns = urlpatterns
+
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="",
+            default_version="v1",
+        ),
+        public=True,
+        urlconf=urlconf,
+    )
+
+    schema_request = rf.get("schema")
+    schema_response = schema_view.without_ui(cache_timeout=0)(
+        request=schema_request, format=".json"
+    )
+
+    schema_response.render()
+    schema = json.loads(schema_response.content.decode())
+    paths = schema["paths"]
+    assert "/view" in paths
+    assert "get" in paths["/view"]
+
+    assert "DCS__Child1" in schema["definitions"]
